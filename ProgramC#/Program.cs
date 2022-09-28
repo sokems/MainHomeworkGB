91a5d2ee25411d04f57e3a1be6f0afb064007c71
Sokem <sokems@mail.ru>
2022-09-28 20:29:27 +0500

    create func NumberOfElementsUpToThreeInLength

--- ProgramC#/Program.cs
+++ ProgramC#/Program.cs
@@ -1,1 +1,13 @@
-﻿string[] array = {"hello", "12", "no", "butter", " "};
+﻿int NumberOfElementsUpToThreeInLength(string[] array)
+{
+    int current = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+            current++;
+    }
+
+    return current;
+}
+
+string[] array = {"hello", "12", "no", "butter", " "};
